e0e7109a259869a0686d001883811ffb77ee0ce3
mblackstock <mikeblackstock@gmail.com>
2016-01-04 16:34:06 -0800

    update noble to 1.2.1 version add scan control input to node fix #6 - multiple messages after multiple deploys

--- noble/node-red-contrib-noble.js
+++ noble/node-red-contrib-noble.js
@@ -90,7 +90,6 @@ module.exports = function(RED) {
         // Take care of starting the scan and sending the status message
         function startScan(stateChange, error) {
             if (!node.scanning) {
-                node.scanning = true;
                 // send status message
                 var msg = {
                     statusUpdate: true,
@@ -100,15 +99,17 @@ module.exports = function(RED) {
                 };
                 node.send(msg);
                 // start the scan
-                noble.startScanning(node.uuids, node.duplicates);
-                node.log("Scanning for BLEs started. UUIDs: " + node.uuids + " - Duplicates allowed: " + node.duplicates);
+                noble.startScanning(node.uuids, node.duplicates, function() {
+                    node.log("Scanning for BLEs started. UUIDs: " + node.uuids + " - Duplicates allowed: " + node.duplicates);
+                    node.status({fill:"green",shape:"dot",text:"started"});
+                    node.scanning = true;
+                });
             }
         }
 
         // Take care of stopping the scan and sending the status message
         function stopScan(stateChange, error) {
             if (node.scanning) {
-                node.scanning = false;
                 // send status message
                 var msg = {
                     statusUpdate: true,
@@ -118,11 +119,13 @@ module.exports = function(RED) {
                 };
                 node.send(msg);
                 // start the scan
-                noble.stopScanning();
+                noble.stopScanning(function() {
+                    node.log('BLE scanning stopped.');
+                    node.status({fill:"red",shape:"ring",text:"stopped"});
+                    node.scanning = false;
+                });
                 if (error) {
                     node.warn('BLE scanning stopped due to change in adapter state.');
-                } else {
-                    node.log('BLE scanning stopped.');
                 }
             }
         }
@@ -157,21 +160,24 @@ module.exports = function(RED) {
 
             node.warn('Unable to start BLE scan. Adapter state: ' + noble.state);
         }
+
+        // control scanning
+        this.on('input', function (msg) {
+            if (msg.scan && msg.scan === true) {
+                startScan(false, false);
+            } else if (msg.scan === false) {
+                stopScan(false, false);
+            }
+        });
     
         this.on("close", function() {
             // Called when the node is shutdown - eg on redeploy.
             // Allows ports to be closed, connections dropped etc.
             // eg: this.client.disconnect();
             stopScan(false, false);
+            // remove listeners since they get added again on deploy
+            noble.removeAllListeners();
         });
-
-        //noble.on('scanStart', function() {
-        //    node.debug("Scan of BLEs started");
-        //});
-        //
-        //noble.on('scanStop', function() {
-        //    node.debug("Scan of BLEs stopped");
-        //});
     }
     
     // Register the node by name. This must be called before overriding any of the
